09faaf398744fa2133ad59e9588bcfa4fec0abe6
Raj Kumar Sanpui <raj.kumar.sanpui@gmail.com>
2020-05-20 00:57:54 +0530

    Add files via upload

---
+++ Classes/HelloWorldScene.h
@@ -0,0 +1,29 @@
+//
+// Created by rsanpui on 5/14/2020.
+//
+
+#ifndef PROJ_ANDROID_PHYSICSCLASS_H
+#define PROJ_ANDROID_PHYSICSCLASS_H
+
+#include "cocos2d.h"
+
+class HelloWorld : public cocos2d::Layer
+{
+public:
+
+    static cocos2d::Scene* createScene();
+    virtual bool init();
+
+    void menuCloseCallback(cocos2d::CCObject* pSender);
+    CREATE_FUNC(HelloWorld);
+private:
+    cocos2d::Sprite* soldierSprite;
+    void addBoulder(float dt);
+    void initializePhysics(cocos2d::Sprite* sprite);
+    void initTouch();
+    void moveSoldier(cocos2d::Touch* toque, cocos2d::Event* event);
+};
+
+
+
+#endif //PROJ_ANDROID_PHYSICSCLASS_H
